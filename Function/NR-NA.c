6b430d67766f8b3b2f82f114437f574a7db7d450
Kavyan Nayak <kavyannayak104@gmail.com>
2024-11-30 13:34:36 +0530

    NR - NA

--- Function/NR-NA.c
+++ Function/NR-NA.c
@@ -3,10 +3,39 @@
 #include <stdio.h>
 #include <conio.h>
 int addfun();
+int subfun();
+int mulfun();
+int divfun();
+
 void main()
 {
-    addfun();
+    int choice;
+    
+    printf("Enter the choice : \n");
+    printf("1. Addition \n");
+    printf("2. Subtraction \n");
+    printf("3. Multiplication \n");
+    printf("4. Division \n");
+    scanf("%d", &choice);
 
+    switch (choice)
+    {
+    case 1:
+        addfun();
+        break;
+    
+    case 2:
+        subfun();
+        break;
+
+    case 3:
+        mulfun();
+        break;
+
+    case 4:
+        divfun();
+        break;
+    }
 }
 
 int addfun()
@@ -24,6 +53,55 @@ int addfun()
     printf("The sum of a and b is : %d", x);
 
     return x;
+}
 
+int subfun()
+{
+    int a,b, x;
 
+    printf("Enter the value of a : ");
+    scanf("%d", &a);
+
+    printf("Enter the value of b : ");
+    scanf("%d", &b);
+
+    x = a - b;
+
+    printf("The sum of a and b is : %d", x);
+
+    return x;
+}
+
+int mulfun()
+{
+    int a,b, x;
+
+    printf("Enter the value of a : ");
+    scanf("%d", &a);
+
+    printf("Enter the value of b : ");
+    scanf("%d", &b);
+
+    x = a * b;
+
+    printf("The sum of a and b is : %d", x);
+
+    return x;
+}
+
+int divfun()
+{
+    int a,b, x;
+
+    printf("Enter the value of a : ");
+    scanf("%d", &a);
+
+    printf("Enter the value of b : ");
+    scanf("%d", &b);
+
+    x = a / b;
+
+    printf("The sum of a and b is : %d", x);
+
+    return x;
 }
