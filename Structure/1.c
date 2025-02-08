10f09b7efffbf87c21a3aeecd900a6c3e79d9523
Kavyan Nayak <kavyannayak104@gmail.com>
2025-02-08 12:18:16 +0530

    Structure

---
+++ Structure/1.c
@@ -0,0 +1,31 @@
+// student data structure
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct students{
+    int Roll_No;
+    char Name[30];
+    int Age;
+};
+
+int main()
+{
+    struct students s1;
+
+    printf("Enter Roll No: ");
+    scanf("%d", &s1.Roll_No);
+
+    printf("Enter Name: ");
+    scanf("%s", s1.Name);
+
+    printf("Enter Age: ");
+    scanf("%d", &s1.Age);
+
+
+    printf("\n\nRoll No: %d\n", s1.Roll_No);
+    printf("Name: %s\n", s1.Name);
+    printf("Age: %d\n", s1.Age);
+
+    return 0;
+}
